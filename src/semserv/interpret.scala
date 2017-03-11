3be25f243d6a80f3329f30d4557f342318581baf
Carsten Hartenfels <hartenfels@uni-koblenz.de>
2017-03-11 22:18:12 +0100

    Consistenize JSON imports

--- src/semserv/interpret.scala
+++ src/semserv/interpret.scala
@@ -1,9 +1,14 @@
 package semserv;
 
 import scala.collection.mutable.HashMap
-import scala.collection.immutable.{Vector => V};
-import spray.json.{deserializationError, JsonParser, JsObject, JsValue}
-import spray.json.{JsArray => JA, JsBoolean => JB, JsString  => JS}
+
+import spray.json.{
+  deserializationError, JsonParser, JsValue,
+  JsArray   => JA,
+  JsBoolean => JB,
+  JsObject  => JO,
+  JsString  => JS
+}
 
 import org.semanticweb.owlapi.model.{
   OWLNamedIndividual          => Nominal,
@@ -32,7 +37,7 @@ object interpret {
     try {
       execute(JsonParser(source))
     } catch {
-      case e: Exception => JsObject(Map("error" -> JS(e.toString())))
+      case e: Exception => JO(Map("error" -> JS(e.toString())))
     }
 
   private def execute(value: JsValue): JsValue =
@@ -74,8 +79,8 @@ object interpret {
 
     def onSame(value: JsValue): JsValue =
       value match {
-        case JA(V(n, m)) => JB(kb.same(onNominal(n), onNominal(m)))
-        case _           => deserializationError("bad same")
+        case JA(Vector(n, m)) => JB(kb.same(onNominal(n), onNominal(m)))
+        case _                => deserializationError("bad same")
       }
 
     def onQuery(value: JsValue): JsValue =
@@ -83,40 +88,40 @@ object interpret {
 
     def onProject(value: JsValue): JsValue =
       value match {
-        case JA(V(n, r)) => ja(kb.project(onNominal(n), onRole(r)))
-        case _           => deserializationError("bad project")
+        case JA(Vector(n, r)) => ja(kb.project(onNominal(n), onRole(r)))
+        case _                => deserializationError("bad project")
       }
 
     def onSubtype(value: JsValue): JsValue =
       value match {
-        case JA(V(c, d)) => JB(kb.subtype(onConcept(c), onConcept(d)))
-        case _           => deserializationError("bad subtype")
+        case JA(Vector(c, d)) => JB(kb.subtype(onConcept(c), onConcept(d)))
+        case _                => deserializationError("bad subtype")
       }
 
     def onMember(value: JsValue): JsValue =
       value match {
-        case JA(V(c, n)) => JB(kb.member(onConcept(c), onNominal(n)))
-        case _           => deserializationError("bad subtype")
+        case JA(Vector(c, n)) => JB(kb.member(onConcept(c), onNominal(n)))
+        case _                => deserializationError("bad subtype")
       }
 
     def onRole(value: JsValue): Role =
       value match {
-        case JA(V(JS("r"), JS(s))) => kb.role(s)
-        case JA(V(JS("i"), v))     => kb.invert(onRole(v))
-        case _                     => deserializationError("bad role")
+        case JA(Vector(JS("r"), JS(s))) => kb.role(s)
+        case JA(Vector(JS("i"), v))     => kb.invert(onRole(v))
+        case _                          => deserializationError("bad role")
       }
 
     def onConcept(value: JsValue): Concept =
       value match {
-        case JB(true)              => kb.everything
-        case JB(false)             => kb.nothing
-        case JA(V(JS("C"), JS(s))) => kb.concept(s)
-        case JA(V(JS("U"), JA(a))) => kb.unify(    a.map(onConcept(_)):_*)
-        case JA(V(JS("I"), JA(a))) => kb.intersect(a.map(onConcept(_)):_*)
-        case JA(V(JS("N"), c))     => kb.negate(onConcept(c))
-        case JA(V(JS("E"), r, c))  => kb.exists(onRole(r), onConcept(c))
-        case JA(V(JS("A"), r, c))  => kb.forall(onRole(r), onConcept(c))
-        case _                     => deserializationError("bad concept")
+        case JB(true)                   => kb.everything
+        case JB(false)                  => kb.nothing
+        case JA(Vector(JS("C"), JS(s))) => kb.concept(s)
+        case JA(Vector(JS("U"), JA(a))) => kb.unify(    a.map(onConcept(_)):_*)
+        case JA(Vector(JS("I"), JA(a))) => kb.intersect(a.map(onConcept(_)):_*)
+        case JA(Vector(JS("N"), c))     => kb.negate(onConcept(c))
+        case JA(Vector(JS("E"), r, c))  => kb.exists(onRole(r), onConcept(c))
+        case JA(Vector(JS("A"), r, c))  => kb.forall(onRole(r), onConcept(c))
+        case _                          => deserializationError("bad concept")
       }
   }
 }
