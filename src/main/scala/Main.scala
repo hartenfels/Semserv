99e5f1f7a6689b242f07936f578c5ceb1ff746b7
Carsten Hartenfels <hartenfels@uni-koblenz.de>
2017-03-12 12:46:05 +0100

    Allow port settage and add usage instructions

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,22 @@
+import scala.util.Properties.envOrElse
+
+
 object Main {
   def main(args: Array[String]): Unit = {
-    semserv.Server(53115)
+    val DefaultPort = 53115
+
+    if (!args.isEmpty) {
+      System.err.println(
+        s"""
+           |I don't take any arguments.
+           |
+           |Set the SEMSERV_PORT environment variable
+           |if you want to use a different port than
+           |the default port $DefaultPort.
+           |""".stripMargin)
+      System.exit(2)
+    }
+
+    semserv.Server(envOrElse("SEMSERV_PORT", s"$DefaultPort").toInt)
   }
 }
